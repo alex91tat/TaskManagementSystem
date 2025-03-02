8723afff24520febe799d170d44375af940d77c2
alex91tat <tatalexandru91@gmail.com>
2025-03-02 13:39:48 +0200

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module utcnpt.pt2025_30422_tat_dragos_assignment_1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens utcnpt.pt2025_30422_tat_dragos_assignment_1 to javafx.fxml;
+    exports utcnpt.pt2025_30422_tat_dragos_assignment_1;
+}
